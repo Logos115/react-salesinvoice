469460a5bb805133b474bcd5d5c207e0692bde71
Logos115 <logos115@outlook.com>
2026-01-14 01:01:31 +0800

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -63,8 +63,8 @@ const theme = createTheme({
   },
 });
 
-axios.defaults.baseURL = "http://localhost:4000/api";
-// axios.defaults.baseURL = "http://blueoceanblue.com/api";
+// axios.defaults.baseURL = "http://localhost:4000/api";
+axios.defaults.baseURL = "https://srv222237.hstgr.cloud/api";
 
 axios.interceptors.request.use(
   function (config) {
